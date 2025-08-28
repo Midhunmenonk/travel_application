92a2e18b75718cde7b1bc6b6105a0999e110006a
Midhun Menon K <midhunmenon4@gmail.com>
2025-08-28 23:35:25 +0530

    Added templates

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm 
 # Create your views here.
+
+
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            messages.success(request, "Registration successful!")
+            return redirect('home') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated successfully!')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'profile.html', {'form': form})
